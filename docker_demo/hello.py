f3a2c3afdfe1fff361397ae9f361b16dc0bf72d9
Sarah <test@test.com>
2026-06-02 15:36:52 +0100

    Day 2 PM

--- docker_demo/hello.py
+++ docker_demo/hello.py
@@ -1,1 +1,20 @@
-print("Hello, I just used Docker!")
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def get_sum(self):
+        return self.num1 + self.num2
+    
+    def get_product(self):
+        return self.num1 * self.num2
+    
+    def get_diff(self):
+        return self.num1 - self.num2
+    
+    def get_quotient(self):
+        return self.num1 / self.num2
+    
+if __name__ == "__main__":
+    myCalc = Calculator(144,12)
+    print(myCalc.get_quotient())
